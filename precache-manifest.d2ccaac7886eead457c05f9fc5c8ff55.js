b9aa5dcced69db6300a1b57de373b1cebc13868d
Jack Perry <jack.perry.2015@my.ntu.ac.uk>
2020-06-21 22:16:03 +0100

    Updates

--- precache-manifest.d2ccaac7886eead457c05f9fc5c8ff55.js
+++ precache-manifest.d2ccaac7886eead457c05f9fc5c8ff55.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "83f8fd28f4f34150179b5bd5b0ceda19",
+    "revision": "c7288dcb7ef3f7c5d887c5a9e7f069b3",
     "url": "/myportfolio/index.html"
   },
   {
-    "revision": "53f86c123b156277b756",
+    "revision": "2ecbe027ff2b61261471",
     "url": "/myportfolio/static/css/2.e4346227.chunk.css"
   },
   {
-    "revision": "801b3eeca33fbedda9df",
+    "revision": "5d48e42808486a035d26",
     "url": "/myportfolio/static/css/main.2535ecbe.chunk.css"
   },
   {
-    "revision": "53f86c123b156277b756",
-    "url": "/myportfolio/static/js/2.bf12959d.chunk.js"
+    "revision": "2ecbe027ff2b61261471",
+    "url": "/myportfolio/static/js/2.8fecd3cd.chunk.js"
   },
   {
     "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
-    "url": "/myportfolio/static/js/2.bf12959d.chunk.js.LICENSE.txt"
+    "url": "/myportfolio/static/js/2.8fecd3cd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "801b3eeca33fbedda9df",
-    "url": "/myportfolio/static/js/main.56f8065c.chunk.js"
+    "revision": "5d48e42808486a035d26",
+    "url": "/myportfolio/static/js/main.cafa4905.chunk.js"
   },
   {
     "revision": "da2e02db1e870a8bb8e5",
